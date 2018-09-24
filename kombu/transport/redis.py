0c706b0becc14fc542a7498ba05907f29d04a006
Nicholas Pilon <npilon@lexmachina.com>
2018-09-24 15:07:02 -0700

    Update kombu to avoid async

--- kombu/transport/redis.py
+++ kombu/transport/redis.py
@@ -812,7 +812,7 @@ class Channel(virtual.Channel):
                                socket_keepalive_options=None, **params):
         return params
 
-    def _connparams(self, async=False, _r210_options=(
+    def _connparams(self, async_=False, _r210_options=(
             'socket_connect_timeout', 'socket_keepalive',
             'socket_keepalive_options')):
         conninfo = self.connection.client
@@ -859,7 +859,7 @@ class Channel(virtual.Channel):
             redis.Connection
         )
 
-        if async:
+        if async_:
             class Connection(connection_cls):
                 def disconnect(self):
                     super(Connection, self).disconnect()
@@ -868,13 +868,13 @@ class Channel(virtual.Channel):
 
         return connparams
 
-    def _create_client(self, async=False):
-        if async:
+    def _create_client(self, async_=False):
+        if async_:
             return self.AsyncClient(connection_pool=self.async_pool)
         return self.Client(connection_pool=self.pool)
 
-    def _get_pool(self, async=False):
-        params = self._connparams(async=async)
+    def _get_pool(self, async_=False):
+        params = self._connparams(async_=async_)
         self.keyprefix_fanout = self.keyprefix_fanout.format(db=params['db'])
         return redis.ConnectionPool(**params)
 
@@ -911,18 +911,18 @@ class Channel(virtual.Channel):
     @property
     def async_pool(self):
         if self._async_pool is None:
-            self._async_pool = self._get_pool(async=True)
+            self._async_pool = self._get_pool(async_=True)
         return self._async_pool
 
     @cached_property
     def client(self):
         """Client used to publish messages, BRPOP etc."""
-        return self._create_client(async=True)
+        return self._create_client(async_=True)
 
     @cached_property
     def subclient(self):
         """Pub/Sub connection used to consume fanout queues."""
-        client = self._create_client(async=True)
+        client = self._create_client(async_=True)
         pubsub = client.pubsub()
         pool = pubsub.connection_pool
         pubsub.connection = pool.get_connection('pubsub', pubsub.shard_hint)
